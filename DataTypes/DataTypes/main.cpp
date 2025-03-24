b0ee304ea9dfc12ee0915b054f1d2619517c3e06
User <User@DESKTOP-0TCDSR6>
2025-03-24 13:03:47 +0300

    DataTypes day 1

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,11 @@
+//DataTypes
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "DataTypes\n";;
+	cout << true << endl;
+	cout << false << endl;
+}
