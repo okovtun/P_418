0a5ee9871ad0c8c8cb4c4eeeb2b7c34c40ddeeb9
User <User@DESKTOP-QHG18FL>
2025-04-28 12:55:04 +0300

    Loops day 1

---
+++ ControlStructures/Calc/main.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+using namespace std;
+
+//switch
+//if...else...
+
+//#define CALC_IF
+#define CALC_SWITCH
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	double a, b;	//числа, вводимые пользователем
+	char s;			//Sign - знак операции
+	cout << "Введите простое выражение: " << "(например 2+3)";
+	cin >> a >> s >> b;
+	/*cin >> a;
+	cin >> s;
+	cin >> b;*/
+	cout << a << s << b << endl;
+#if defined CALC_IF
+	if (s == '+')
+	{
+		cout << a << " + " << b << " = " << a + b << endl;
+	}
+	else if (s == '-')
+	{
+		cout << a << " - " << b << " = " << a - b << endl;
+	}
+	else if (s == '*')
+	{
+		cout << a << " * " << b << " = " << a * b << endl;
+	}
+	else if (s == '/')
+	{
+		cout << a << " / " << b << " = " << a / b << endl;
+	}
+	else
+	{
+		cout << "Error: No operation" << endl;
+	}
+#endif
+
+#if defined CALC_SWITCH
+
+	switch (s)
+	{
+	case '+':cout << a << " + " << b << " = " << a + b << endl; break;
+	case '-':cout << a << " - " << b << " = " << a - b << endl; break;
+	case '*':cout << a << " * " << b << " = " << a * b << endl; break;
+	case '/':cout << a << " / " << b << " = " << a / b << endl; break;
+	default: cout << "Error: No operation" << endl;
+	}
+	//case - случай
+	//label - метка
+	//value - значение
+	//already - уже
+#endif
+}
---
+++ ControlStructures/Loops/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<conio.h>
+using namespace std;
+
+//#define WHILE_1
+//#define WHILE_2
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#ifdef WHILE_1
+	int i = 0;	//Счетчик цикла
+	int n;		//Количество итераций
+	cout << "Введите количество итераций: ";
+	cin >> n;
+	while (++i < n)
+	{
+		cout << i << " Loops" << endl;
+		//i++;
+	}
+#endif // WHILE_1
+
+#ifdef WHILE_2
+	int n;
+	cout << "Введите количество итераций: "; cin >> n;
+	while (n--)
+	{
+		cout << n << "\t";
+	}
+	cout << endl;
+#endif // WHILE_2
+
+	char key;	//эта переменная будет хранить код нажатой клавиши.
+	do
+	{
+		key = _getch();	//Функция _getch() ожидает нажатие клавиши,
+						//и возвращает ASCII-код нажатой клавиши.
+						//Функция _getch() объявлена в библиотеке <conio.h>
+		cout << (int)key << "\t" << key << endl;
+		//(int)key - явное преобразование переменной 'key' в тип данных 'int',
+		//для того чтобы увидеть чиловой код нажатой клавиши.
+	} while (true);
+}
