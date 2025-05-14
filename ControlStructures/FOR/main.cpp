9b4f5c0c2a840e12d76d31ec7a4b734d7c370b5a
User <User@DESKTOP-QHG18FL>
2025-05-14 13:39:17 +0300

    FOR day 2

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -1,19 +1,51 @@
 ﻿#include<iostream>
 using namespace std;
 
+//#define FACTORIAL
+//#define POWER
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	
-	int n = 5;
-	
-	for (int i = 0;
-		i < n;
-		i++)
+#ifdef FACTORIAL
+	int n;
+	cout << "Введите число: "; cin >> n;
+	double f = 1;
+	for (int i = 1; i <= n; i++)
 	{
-		cout << i << "\t";
+		cout << i << "! = ";
+		f *= i;
+		cout << f << "\n";
 	}
 	cout << endl;
+#endif // FACTORIAL
+
+#ifdef POWER
+	double a;	//основание степени
+	int n;	//показатель степени
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	double N = 1;
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	cout << N << endl;
+	main();
+#endif // POWER
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (i % 16 == 0)cout << endl;
+		cout << (char)i << " ";
+	}
+
 }
 /*
 --------------------------------------------
