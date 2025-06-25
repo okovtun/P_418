39fc4cb1b026307ed3e8f84561e3254489edcc4c
User <User@DESKTOP-QHG18FL>
2025-06-25 12:59:14 +0300

    StaticLIB DONE

---
+++ Functions/CheckLIB/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<FillRand.h>
+#include<Print.h>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+}
